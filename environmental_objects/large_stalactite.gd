2813ca4a13a1edbd0ea1d090769782fa88b84afb
Wolfenrahd <willfarr440@gmail.com>
2025-01-29 19:03:19 -0500

    large stalactite

---
+++ environmental_objects/large_stalactite.gd
@@ -0,0 +1,43 @@
+extends StaticBody2D
+
+@onready var animation_player = $animation_player
+@onready var ground_cast = $ground_cast
+@onready var sprite_2d = $sprite_2d
+
+@export var health := 3
+@export var gravity := 90.0
+
+var falling := false
+var landed := false
+
+var velocity := Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if falling:
+		velocity.y += gravity * delta
+		position += velocity * delta
+		if ground_cast.is_colliding():
+			falling = false
+			landed = true
+			
+			position.y = ground_cast.get_collision_point().y - sprite_2d.get_rect().size.y / 2 + 16
+
+func _input(event):
+	if event.is_action_pressed("cannon"):
+		falling = true
+
+func _on_hurtbox_hurt(hitbox, damage):
+	if landed:
+		return
+	
+	health -= damage
+	if health <= 0:
+		falling = true
+	else:
+		animation_player.play("shake")

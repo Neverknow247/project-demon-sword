3bac7edd5e94a7331b78d98137c0ac5555fc186a
Nathan Bell <71198973+Neverknow247@users.noreply.github.com>
2025-01-31 01:31:30 -0600

    add levels, start player cannon, add save station and door connections

---
+++ player/cannon.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var rotation_point = $rotation_point
+
+#func _physics_process(delta):
+	#rotate_cannon()
+#
+#func rotate_cannon():
+	#if Input.is_action_pressed("up") || Input.is_action_pressed("c_up"):
+		#rotation_point.rotation_degrees = -90
+	#elif Input.is_action_pressed("up") || Input.is_action_pressed("c_up"):
+		#rotation_point.rotation_degrees = 90
+	#else:
+		#rotation_point.rotation_degrees = 0

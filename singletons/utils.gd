1484be6ca15692e392fdeedede2df7b90e714c35
Nathan Bell <71198973+Neverknow247@users.noreply.github.com>
2025-02-08 17:35:07 -0600

    add volume menu and fix a few bugs

--- singletons/utils.gd
+++ singletons/utils.gd
@@ -5,18 +5,18 @@ var stats = Stats
 var volume_settings = {
 	"master_volume" = .5,
 	"music_volume" = 1,
-	"sfx_volume" = 1,
+	"sounds_volume" = 1,
 	"voice_volume" = 1
 }
 
 const master_bus_name = "Master"
 const music_bus_name = "Music"
-const sfx_bus_name = "SFX"
+const sounds_bus_name = "Sounds"
 const voice_bus_name = "Voice"
 
 @onready var master_bus = AudioServer.get_bus_index(master_bus_name)
 @onready var music_bus = AudioServer.get_bus_index(music_bus_name)
-@onready var sfx_bus = AudioServer.get_bus_index(sfx_bus_name)
+@onready var sounds_bus = AudioServer.get_bus_index(sounds_bus_name)
 @onready var voice_bus = AudioServer.get_bus_index(voice_bus_name)
 
 func _ready():
@@ -26,7 +26,7 @@ func _ready():
 func set_volume():
 	AudioServer.set_bus_volume_db(master_bus, linear_to_db(volume_settings["master_volume"]))
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(volume_settings["music_volume"]))
-	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(volume_settings["sfx_volume"]))
+	AudioServer.set_bus_volume_db(sounds_bus, linear_to_db(volume_settings["sounds_volume"]))
 	AudioServer.set_bus_volume_db(voice_bus, linear_to_db(volume_settings["voice_volume"]))
 
 var squash_and_stretch = true
